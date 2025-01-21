f4ef78a0550a1d9184c50c89fb88ca445b8c2016
AlexisAlicea <alexis.alicea@agrovizion.com.mx>
2025-01-20 23:08:20 -0800

    Add Form whit documentation odoo

--- commit-helper-odoo/extension.js
+++ commit-helper-odoo/extension.js
@@ -1,36 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const { execSync } = require('child_process');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    let form = vscode.commands.registerCommand('commit-helper.createCommit', async () => {
+        try {
+            const action = await vscode.window.showQuickPick(
+                [
+                  { label: 'FIX', description: 'For bug fixes: mostly used in stable versions but also valid if fixing a recent bug in the development version.' },
+                  { label: 'REF', description: 'For refactoring: when a feature is heavily rewritten.' },
+                  { label: 'ADD', description: 'For adding new modules or features.' },
+                  { label: 'REM', description: 'For removing resources: removing dead code, views, modules, etc.' },
+                  { label: 'REV', description: 'For reverting commits: if a commit causes issues or is unwanted, it is reverted using this tag.' },
+                  { label: 'MOV', description: 'For moving files: use git move and do not change the content of the moved file, otherwise Git may lose track of the file’s history.' },
+                  { label: 'REL', description: 'For release commits: new major or minor stable versions.' },
+                  { label: 'IMP', description: 'For improvements: most changes in the development version are incremental improvements not related to another tag.' },
+                  { label: 'MERGE', description: 'For merge commits: used in forward port of bug fixes or as the main commit for a feature involving several separated commits.' },
+                  { label: 'CLA', description: 'For signing the Odoo Individual Contributor License.' },
+                  { label: 'I18N', description: 'For changes in translation files.' },
+                  { label: 'PERF', description: 'For performance patches.' }
+                ],
+                { placeHolder: 'Select the commit action' }
+              );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commit-helper-odoo" is now active!');
+            if (!action) {
+                vscode.window.showWarningMessage('Cancel Action.');
+                return;
+            }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('commit-helper-odoo.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+            const module = await vscode.window.showInputBox({
+                prompt: 'Which module is affected by this change?',
+                placeHolder: 'For example: auth, database, ui',
+            });
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Commit Helper Odoo!');
-	});
+            if (!module) {
+                vscode.window.showWarningMessage('Cancel Module.');
+                return;
+            }
 
-	context.subscriptions.push(disposable);
+            const description = await vscode.window.showInputBox({
+                prompt: 'Briefly describe the changes made (maximum 80 characters)',
+                placeHolder: 'Enter the description',
+                validateInput: (text) => {
+                    return text.length > 80
+                        ? `The description must not exceed 80 characters (${text.length})`
+                        : null;
+                },
+            });
+            
+
+            if (!description) {
+                vscode.window.showWarningMessage('Cancel Description.');
+                return;
+            }
+
+            const commitMessage = `[${action}] ${module}: ${description}`;
+
+            execSync(`git commit -m "${commitMessage}"`, {
+                cwd: vscode.workspace.rootPath,
+            });
+
+            vscode.window.showInformationMessage(`Create Commit: ${commitMessage}`);
+        } catch (error) {
+            vscode.window.showErrorMessage(`Error Create Commit: ${error.message}`);
+        }
+    });
+
+    context.subscriptions.push(form);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate,
+};
